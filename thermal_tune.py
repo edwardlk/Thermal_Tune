c7afe7c8ff5cdb1686364b64297ce487ba00cc1f
Ed Kramkowski <e.kramkowski@gmail.com>
2014-03-23 17:47:06 -0400

    Change function to Lorentizian, edit test code accordingly

--- thermal_tune.py
+++ thermal_tune.py
@@ -1,6 +1,6 @@
 ################################################################################
 ##
-##
+##                             Thermal Tune from PSD
 ##
 ##
 
@@ -8,20 +8,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-def func(x, a, b, c):
-    return a*np.exp(-b*x) + c
+def func(w, a, b, c):
+    return a/((w-53)**2 + b) + c
 
-x = np.linspace(0,4,50)
-y = func(x, 2.5, 1.3, 0.5)
-yn = y + 0.2*np.random.normal(size=len(x))
+def stiffness(T, A, B):
+    k_B = 1.3806 * 10**-23
+    C = 0.8417
+    return C*k_B*T*np.sqrt(B)/(np.pi*A)
 
-popt, pcov = curve_fit(func, x, yn)
+w = np.linspace(0,100,200)
+y = func(w, 10, 2, 1)
+yn = y + 0.2*np.random.normal(size=len(w))
 
-print "popt = ", popt
-print "pcov = ", pcov
+popt, pcov = curve_fit(func, w, yn)
 
 plt.figure()
-plt.plot(x, yn, 'r.')
-plt.plot(x, func(x, popt[0], popt[1], popt[2]), 'b')
+plt.plot(w, yn, 'r.')
+plt.plot(w, func(w, popt[0], popt[1], popt[2]), 'b')
 
 plt.show()
+
+print "stiffness is ", stiffness(300, popt[0], popt[1])
