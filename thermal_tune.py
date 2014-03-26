4222258fe910e50d759c74b239559844f4b4bf8c
Ed Kramkowski <e.kramkowski@gmail.com>
2014-03-26 15:39:44 -0400

    add input for resonance frequency, 'lambda' func to sub res. freq.

--- thermal_tune.py
+++ thermal_tune.py
@@ -2,29 +2,33 @@
 ##
 ##                             Thermal Tune from PSD
 ##
-##
+## 22 header lines on labview data files
 
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-def func(w, a, b, c):
-    return a/((w-53)**2 + b) + c
+def func(w, a, b, c, w_0):
+    return a/((w-w_0)**2 + b) + c
 
 def stiffness(T, A, B):
     k_B = 1.3806 * 10**-23
     C = 0.8417
     return C*k_B*T*np.sqrt(B)/(np.pi*A)
 
+w_0 = float(input("Enter the resonance frequency in kHz: "))
+
 w = np.linspace(0,100,200)
-y = func(w, 10, 2, 1)
+y = func(w, 10, 2, 1, 53)
 yn = y + 0.2*np.random.normal(size=len(w))
 
-popt, pcov = curve_fit(func, w, yn)
+tst = lambda x, a, b, c: func(x, a, b, c, w_0)
+
+popt, pcov = curve_fit(tst, w, yn)
 
 plt.figure()
 plt.plot(w, yn, 'r.')
-plt.plot(w, func(w, popt[0], popt[1], popt[2]), 'b')
+plt.plot(w, tst(w, popt[0], popt[1], popt[2]), 'b')
 
 plt.show()
 
