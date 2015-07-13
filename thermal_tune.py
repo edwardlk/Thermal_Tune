57eb27a8439069821a885d9b6d31e3476008f759
edwardlk <e.kramkowski@gmail.com>
2015-07-13 15:14:27 -0400

    more work

--- thermal_tune.py
+++ thermal_tune.py
@@ -12,26 +12,36 @@ def func(w, a, b, c, w_0):
     return a/((w-w_0)**2 + b) + c
 
 def stiffness(T, A, B):
-    k_B = 1.3806 * 10**-23
-    C = 0.8417
-    return C*k_B*T*np.sqrt(B)/(np.pi*A)
+	k_B = 1.3806 * 10**-23
+	C = 0.8417
+	sens = 50
+	s = 10**-7 / sens
+	return C*k_B*T*np.sqrt(B)/(np.pi*A*s**2)
 
 data = np.genfromtxt("C:\Users\ADMIN\Desktop\Waveform01.txt", skip_header=1)
-data_t = data.transpose()
-freq = data_t[0]
-FFT_dBV = data_t[1]
+rows = data.shape[0]
+columns = data.shape[1]
+
+freq = np.zeros(rows)
+FFT_dBV = np.zeros(rows)
+FFT_V = np.zeros(rows)
+PSD_V2 = np.zeros(rows)
+
+for x1 in range(0, rows):
+	freq[x1] = data[x1, 0]
+	FFT_dBV[x1] = data[x1, 1]
 
 F_s = 200000
-N = 2000
-
-FFT_V = np.zeros(len(FFT_dBV))
-PSD_V2 = np.zeros(len(FFT_dBV))
+N = 200
 
 for x in range(len(FFT_dBV)):
 	FFT_V[x] = 10**(FFT_dBV[x]/20)
 	PSD_V2[x] = abs(FFT_V[x])**2 / (F_s * N)
 	
 peak = np.argmax(PSD_V2[10:])+10
+
+x_vals = freq[(peak-30):(peak+30)]
+y_vals = PSD_V2[(peak-30):(peak+30)] * 10**10
 
 tst = lambda x, a, b, c: func(x, a, b, c, freq[peak])
 popt, pcov = curve_fit(tst, freq, PSD_V2)
@@ -42,8 +52,10 @@ plt.plot(freq, tst(freq, popt[0], popt[1], popt[2]), 'b')
 plt.show()
 
 # plt.figure()
-# plt.plot(freq[(peak-20):(peak+20)], PSD_V2[(peak-20):(peak+20)], 'r.')
+# plt.plot(freq[(peak-30):(peak+30)], PSD_V2[(peak-30):(peak+30)], 'r.')
 # plt.show()
+
+print "stiffness is ", stiffness(300, popt[0], popt[1])
 
 # floor = min(yn)
 # PSD = yn - floor
